f411b72b32e066da1f873eb898f770185d0a8356
krispo <konstantin.skipor@gmail.com>
2016-02-16 20:48:26 +0300

    added some tests

--- test/yarrow-test.js
+++ test/yarrow-test.js
@@ -1,12 +1,46 @@
-var tape = require('tape'),
+var test = require('tape'),
+    jsdom = require("jsdom"),
     yarrow = require('../');
 
-tape('Yarrow tests', function(t){
-  t.deepEqual('yarrow', 'yarrow');
+test('Yarrow tests', function(t){
+  var document = jsdom.jsdom("<body>");
+
+  t.test('add single arrow', function(t){
+    var ya = new yarrow.Yarrow();
+    ya.arrow({}, document.body);
+    t.equal(ya.arrows().length, 1);
+    t.end();
+  });
+
+  t.test('add 2 arrows successively with .arrow()', function(t){
+    var ya = new yarrow.Yarrow();
+    ya.arrow({}, document.body);
+    ya.arrow({}, document.body);
+    t.equal(ya.arrows().length, 2);
+    t.end();
+  });
+
+  t.test('add 3 arrows simultaneously with .arrows()', function(t){
+    var ya = new yarrow.Yarrow();
+    ya.arrows([{}, {}, {}], document.body);
+    t.equal(ya.arrows().length, 3);
+    t.end();
+  });
+
   t.end();
 })
 
-tape('Arrow tests', function(t){
-  t.equal('arrow', 'arrow');
+test('Arrow tests', function(t){
+  var document = jsdom.jsdom("<body>");
+
+  t.test('expose() should remove arrow globally', function(t){
+    var ya = new yarrow.Yarrow();
+    var a = ya.arrow({}, document.body);
+    t.equal(ya.arrows().length, 1);
+    a.dispose();
+    t.equal(ya.arrows().length, 0);
+    t.end();
+  });
+
   t.end();
 })

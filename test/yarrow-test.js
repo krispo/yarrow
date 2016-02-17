8b3b727b228ef0954d25f194e4e40d4dd9f8089d
krispo <konstantin.skipor@gmail.com>
2016-02-17 17:10:01 +0300

    updated tests

--- test/yarrow-test.js
+++ test/yarrow-test.js
@@ -1,32 +1,33 @@
 var test = require('tape'),
     jsdom = require("jsdom"),
-    yarrow = require('../');
-var d3s = require('d3-selection');
+    yarrow = require('../')
+  ;
+require('d3-selection');
 require('d3-selection-multi');
 
 test('Yarrow tests', function(t){
-  var document = jsdom.jsdom("<body>");
-  var body = document.body;
-  body.appendChild(document.createElement("svg"))
 
   t.test('add single arrow', function(t){
+    var document = jsdom.jsdom("<body>");
     var ya = new yarrow.Yarrow();
-    var a = ya.arrow({}, body);
+    ya.arrow({}, document.body);
     t.equal(ya.arrows().length, 1);
     t.end();
   });
 
   t.test('add 2 arrows successively with .arrow()', function(t){
+    var document = jsdom.jsdom("<body>");
     var ya = new yarrow.Yarrow();
-    ya.arrow({}, body);
-    ya.arrow({}, body);
+    ya.arrow({}, document.body);
+    ya.arrow({}, document.body);
     t.equal(ya.arrows().length, 2);
     t.end();
   });
 
   t.test('add 3 arrows simultaneously with .arrows()', function(t){
+    var document = jsdom.jsdom("<body>");
     var ya = new yarrow.Yarrow();
-    ya.arrows([{}, {}, {}], body);
+    ya.arrows([{}, {}, {}], document.body);
     t.equal(ya.arrows().length, 3);
     t.end();
   });
@@ -35,23 +36,35 @@ test('Yarrow tests', function(t){
 })
 
 test('Arrow tests', function(t){
-  var document = jsdom.jsdom("<body>");
-  var body = document.body;
 
   t.test('.render() should create an arrow', function(t){
+    var document = jsdom.jsdom("<body>");
     var ya = new yarrow.Yarrow();
-    var a = ya.arrow({}, body);
+    var a = ya.arrow({}, document.body);
     a.render();
     t.equal(document.querySelectorAll('.yarrow').length, 1);
     t.end();
   });
 
   t.test('.dispose() should remove arrow globally', function(t){
+    var document = jsdom.jsdom("<body>");
     var ya = new yarrow.Yarrow();
-    var a = ya.arrow({}, body);
+    var a = ya.arrow({}, document.body);
     t.equal(ya.arrows().length, 1);
     a.dispose();
     t.equal(ya.arrows().length, 0);
+    t.end();
+  });
+
+  t.test('.dispose() should remove arrow from the DOM after rendering', function(t){
+    var document = jsdom.jsdom("<body>");
+    var ya = new yarrow.Yarrow();
+    var a = ya.arrow({}, document.body);
+    t.equal(document.querySelectorAll('.yarrow').length, 0);
+    a.render();
+    t.equal(document.querySelectorAll('.yarrow').length, 1);
+    a.dispose();
+    t.equal(document.querySelectorAll('.yarrow').length, 0);
     t.end();
   });
 

e9d99e4293a15296e5f85e9c30a30b3940d4a508
krispo <konstantin.skipor@gmail.com>
2016-02-27 22:25:31 +0300

    added reverse text direction example

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var ex = [
   'ex_basic',
+  'ex_reverse_text_direction',
   'ex_styles'
 ];
 document.addEventListener('DOMContentLoaded', function main() {
@@ -47,6 +48,21 @@ function ex_styles(el){
         'font-size': 20,
         'font-weight': 'bold'
       }
+    }, el)
+    .render();
+}
+
+function ex_reverse_text_direction(el){
+  el.innerHTML = '';
+  var ya = new yarrow.Yarrow();
+  ya.arrow({
+      x: 100,
+      y: 220,
+      dx: 200,
+      dy: -140,
+      text: "I'm arrow!",
+      textReverseDirection: true,
+      textStartOffset: 70
     }, el)
     .render();
 }

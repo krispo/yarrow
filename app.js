012301465cd097d25fb0878819e6d028bb51a56c
krispo <konstantin.skipor@gmail.com>
2016-02-28 12:50:43 +0300

    added text offset ex

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+var d3s;
 var ex = [
   'ex_basic',
+  'ex_text_start_offset',
+  'ex_text_xy_offset',
   'ex_reverse_text_direction',
   'ex_styles'
 ];
 document.addEventListener('DOMContentLoaded', function main() {
   hljs.initHighlightingOnLoad();
+
+  d3s = d3_selection;
 
   ex.forEach(function(id){
     var el = document.getElementById(id + '_output');
@@ -28,6 +33,61 @@ function ex_basic(el){
       text: "I'm arrow!"
     }, el)
     .render();
+
+  var s = document.createElement('div');
+  s.setAttribute('class', 'coords');
+  s.setAttribute('style', 'top:225px;left:74px;');
+  s.innerHTML = '(100, 220)';
+  var t = document.createElement('div');
+  t.setAttribute('class', 'coords');
+  t.setAttribute('style', 'top:60px;left:242px;');
+  t.innerHTML = '(100 + 200, 220 - 140)';
+  el.appendChild(s);
+  el.appendChild(t);
+}
+
+function ex_text_start_offset(el){
+  el.innerHTML = '';
+  var ya = new yarrow.Yarrow();
+  ya.arrow({
+      x: 100,
+      y: 220,
+      dx: 200,
+      dy: -140,
+      text: "I'm arrow!",
+      textStartOffset: 80
+    }, el)
+    .render();
+}
+
+function ex_text_xy_offset(el){
+  el.innerHTML = '';
+  var ya = new yarrow.Yarrow();
+  ya.arrow({
+      x: 100,
+      y: 220,
+      dx: 200,
+      dy: -140,
+      text: "I'm arrow!",
+      textDx: 80,
+      textDy: 20
+    }, el)
+    .render();
+}
+
+function ex_reverse_text_direction(el){
+  el.innerHTML = '';
+  var ya = new yarrow.Yarrow();
+  ya.arrow({
+      x: 100,
+      y: 220,
+      dx: 200,
+      dy: -140,
+      text: "I'm arrow!",
+      textReverseDirection: true,
+      textStartOffset: 70
+    }, el)
+    .render();
 }
 
 function ex_styles(el){
@@ -48,21 +108,6 @@ function ex_styles(el){
         'font-size': 20,
         'font-weight': 'bold'
       }
-    }, el)
-    .render();
-}
-
-function ex_reverse_text_direction(el){
-  el.innerHTML = '';
-  var ya = new yarrow.Yarrow();
-  ya.arrow({
-      x: 100,
-      y: 220,
-      dx: 200,
-      dy: -140,
-      text: "I'm arrow!",
-      textReverseDirection: true,
-      textStartOffset: 70
     }, el)
     .render();
 }
